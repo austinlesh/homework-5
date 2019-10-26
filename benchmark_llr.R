91d8ae5284951568afe7517618b640a9a5778968
Austin Lesh <austinlesh@gmail.com>
2019-10-26 19:08:03 -0400

    added benchmark code

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,1 +1,7 @@
-ECHO is on.
+## ECHO is on.
+
+source("llr_functions.R")
+
+run_time <- microbenchmark::microbenchmark(llr(x, y, z, omega), times = 100)
+
+cat("llr function runs in", mean(run_time$time), "nanoseconds on average")
