bf1c2737c52900ff73bc3e6fd81fbd7d930e4aa0
Austin Lesh <austinlesh@gmail.com>
2019-10-26 19:04:33 -0400

    filled out llr_functions and test_llr scripts

--- llr_functions.R
+++ llr_functions.R
@@ -1,1 +1,40 @@
-ECHO is on.
+## ECHO is on.
+
+n = 15
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+omega = 1
+
+llr = function(x, y, z, omega) {
+  fits = sapply(z, compute_f_hat, x, y, omega)
+  return(fits)
+}
+
+compute_f_hat = function(z, x, y, omega) {
+  Wz = make_weight_matrix(z, x, omega)
+  X = make_predictor_matrix(x)
+  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  return(f_hat)
+}
+
+make_weight_matrix = function(z, x, omega) {
+  r = abs(x - z) / omega
+  W_vec = sapply(r, W)
+  Wz = diag(W_vec)
+  return(Wz)
+}
+
+W = function(r) {
+  if (abs(r) < 1) {
+    return((1 - (abs(r))^3)^3)
+  }
+  else {
+    return(0)
+  }
+}
+  
+make_predictor_matrix = function(x) {
+  X = cbind(rep(1, length(x)), x)
+  return(X)
+}
--- test_llr.R
+++ test_llr.R
@@ -1,1 +1,36 @@
-ECHO is on.
+## ECHO is on.
+
+library(matrixcalc)
+context("Check local linear regression function")
+source("llr_functions.R")
+
+n = 15
+## a very simple regression model
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+
+test_that("llr output has correct length", {
+    expect_equal(length(llr(x, y, z, omega = 1)), length(z))
+})
+
+test_that("make_weight_matrix returns diagonal matrix", {
+    expect_true(is.diagonal.matrix(make_weight_matrix(z, x, omega = 1)))
+})
+
+test_that("weight matrix all positive", {
+  expect_true(all(make_weight_matrix(x, z, omega = 1) >= 0))
+})
+
+test_that("simple case of make_weight_matrix", {
+  expect_equal(make_weight_matrix(z = 0, x = c(1, 0.5), omega = 1), matrix(c(0, 0, 0, 343/512), nrow = 2, ncol = 2, byrow = FALSE))
+})
+
+test_that("first column of predictor matrix all 1's", {
+  expect_true(all(make_predictor_matrix(x)[, 1] == 1))
+})
+
+test_that("correct nx2 dimensions of predictor matrix", {
+  expect_true(nrow(make_predictor_matrix(x)) == length(x))
+  expect_true(ncol(make_predictor_matrix(x)) == 2)
+})
